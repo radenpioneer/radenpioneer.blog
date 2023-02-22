a28f5c27c05c52dae8d6264ee474850907264a19
radenpioneer <radenpioneer@users.noreply.github.com>
2023-02-22 08:56:59 +0700

    implement top navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import Icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    plugins: [Icons({ compiler: 'jsx', jsx: 'react' })],
+  },
+})
---
+++ src/components/ui/nav.tsx
@@ -0,0 +1,50 @@
+import type { FC } from 'react'
+import MenuIcon from '~icons/material-symbols/menu-rounded'
+import style from './nav.module.scss'
+
+const navLinks = [
+  {
+    title: 'Home',
+    path: '#',
+  },
+  {
+    title: 'About',
+    path: '#',
+  },
+  {
+    title: 'Projects',
+    path: '#',
+  },
+  {
+    title: 'Contact',
+    path: '#',
+  },
+]
+
+const Nav: FC = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">
+            <b>R. Ilham Sastronegoro</b>
+          </a>
+        </li>
+      </ul>
+      <ul className={style.__d_menu}>
+        {navLinks.map((item, i) => (
+          <li key={i}>
+            <a href={item.path}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+      <ul className={style.__m_menu}>
+        <li>
+          <MenuIcon />
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
