04cc0ef4987ffcbcd334d64510ceadc5761d6bba
radenpioneer <radenpioneer@users.noreply.github.com>
2023-02-23 23:04:09 +0700

    add site base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import image from '@astrojs/image'
+import mdx from '@astrojs/mdx'
 import Icons from 'unplugin-icons/vite'
 
 // https://astro.build/config
-import mdx from '@astrojs/mdx'
-
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://radenpioneer.blog',
   integrations: [
     react(),
     image({
