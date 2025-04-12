408e56d2c3ed09aa9f4f8416dac32afa9967027b
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-04-12 13:28:48 +0000

    switch to pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,28 +3,23 @@ import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import icons from 'unplugin-icons/vite'
-
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4321',
   integrations: [sitemap()],
-
   image: {
     experimentalLayout: 'responsive',
     domains: ['astro.badg.es']
   },
-
   vite: {
     plugins: [tailwindcss(), icons({ compiler: 'astro' })]
   },
-
   experimental: {
     responsiveImages: true,
     svg: true
   },
-
   adapter: cloudflare({
     platformProxy: {
       enabled: true
