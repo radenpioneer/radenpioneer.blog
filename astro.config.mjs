b449b0da4e8bc9ca64d56e9ccda9ae189fb26b0d
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-04-12 15:07:22 +0000

    Remove keystatic (implementation moved to another branch)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
-import keystatic from '@keystatic/astro'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import icons from 'unplugin-icons/vite'
@@ -10,7 +9,7 @@ import cloudflare from '@astrojs/cloudflare'
 // https://astro.build/config
 export default defineConfig({
   site: 'http://localhost:4321',
-  integrations: [react(), keystatic(), sitemap()],
+  integrations: [react(), sitemap()],
   image: {
     experimentalLayout: 'responsive',
     domains: ['astro.badg.es']
