61e01477a313ca6ee26ff1fd0bd5730c27fa763f
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-12-22 12:21:04 +0000

    reset layout

---
+++ prettier.config.cjs
@@ -0,0 +1,6 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  // i am just using the standard config, change if you need something else
+  ...require('prettier-config-standard'),
+  plugins: [require.resolve('prettier-plugin-tailwindcss')]
+}
