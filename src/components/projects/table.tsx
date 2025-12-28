eaa09948e58bee923e0d1a5dec23a486daecee8f
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2025-12-28 16:46:25 +0000

    improve table presentation

--- src/components/projects/table.tsx
+++ src/components/projects/table.tsx
@@ -1,21 +1,27 @@
-import { type FC, type TableHTMLAttributes, useState } from 'react'
-import type { CollectionEntry } from 'astro:content'
+import { type FC, useState, useEffect } from 'react'
+import { type CollectionEntry } from 'astro:content'
 import {
   type AccessorKeyColumnDef,
+  type DisplayColumnDef,
+  type SortingState,
   useReactTable,
   getCoreRowModel,
   getSortedRowModel,
-  flexRender,
-  type DisplayColumnDef,
-  type SortingState
+  flexRender
 } from '@tanstack/react-table'
+import {
+  type HTMLMotionProps,
+  motion,
+  stagger,
+  AnimatePresence
+} from 'motion/react'
 import { clsx } from 'clsx/lite'
 import SortDownIcon from '~icons/material-symbols/keyboard-arrow-down'
 import SortUpIcon from '~icons/material-symbols/keyboard-arrow-up'
 
 export type Project = CollectionEntry<'projects'>['data']
 
-export interface TableProps extends TableHTMLAttributes<HTMLTableElement> {
+export interface TableProps extends HTMLMotionProps<'table'> {
   data: Array<Project>
   columns: Array<
     AccessorKeyColumnDef<Project, any> | DisplayColumnDef<Project, any>
@@ -31,6 +37,7 @@ const Table: FC<TableProps> = ({
   ...props
 }) => {
   const [sorting, setSorting] = useState<SortingState>(defaultSorting)
+  const [visible, setVisible] = useState<boolean>(true)
 
   const table = useReactTable({
     data,
@@ -44,56 +51,127 @@ const Table: FC<TableProps> = ({
     enableSortingRemoval: false
   })
 
+  useEffect(() => {
+    const handleTableVisibility = () => {
+      setVisible(false)
+    }
+
+    document.addEventListener('astro:before-preparation', handleTableVisibility)
+    return () => {
+      document.removeEventListener(
+        'astro:before-preparation',
+        handleTableVisibility
+      )
+    }
+  }, [])
+
   return (
-    <table
-      className={clsx(
-        'table-auto w-full border-collapse border-2 border-text-primary/50',
-        className
-      )}
-      {...props}
-    >
-      <thead>
-        {table.getHeaderGroups().map((hGroup) => (
-          <tr className='bg-text-primary/25' key={hGroup.id}>
-            {hGroup.headers.map((header) => (
-              <th
-                className={clsx(
-                  'border-2 border-text-primary/50 p-4',
-                  header.column.getCanSort() && 'cursor-pointer'
-                )}
-                onClick={header.column.getToggleSortingHandler()}
-                key={header.id}
-              >
-                {!header.isPlaceholder && (
-                  <div className='flex items-center'>
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
+    <AnimatePresence>
+      {visible && (
+        <motion.table
+          className={clsx(
+            'table-auto w-full border-collapse border-2 border-text-primary/50',
+            className
+          )}
+          initial='initial'
+          animate='current'
+          exit='exit'
+          variants={{
+            initial: {
+              opacity: 0,
+              y: 25
+            },
+            current: {
+              opacity: 1,
+              y: 0,
+              transition: {
+                duration: 0.08
+              }
+            },
+            exit: {
+              opacity: 0,
+              y: 25,
+              transition: {
+                duration: 0.08
+              }
+            }
+          }}
+          {...props}
+        >
+          <thead>
+            {table.getHeaderGroups().map((hGroup) => (
+              <tr className='bg-text-primary/25' key={hGroup.id}>
+                {hGroup.headers.map((header) => (
+                  <th
+                    className={clsx(
+                      'border-2 border-text-primary/50 p-4',
+                      header.column.getCanSort() && 'cursor-pointer'
                     )}
-                    {header.column.getIsSorted() === 'desc' ? (
-                      <SortDownIcon />
-                    ) : header.column.getIsSorted() === 'asc' ? (
-                      <SortUpIcon />
-                    ) : null}
-                  </div>
-                )}
-              </th>
+                    onClick={header.column.getToggleSortingHandler()}
+                    key={header.id}
+                  >
+                    {!header.isPlaceholder && (
+                      <div className='flex items-center'>
+                        {flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                        {header.column.getIsSorted() === 'desc' ? (
+                          <SortDownIcon />
+                        ) : header.column.getIsSorted() === 'asc' ? (
+                          <SortUpIcon />
+                        ) : null}
+                      </div>
+                    )}
+                  </th>
+                ))}
+              </tr>
             ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.map((row) => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map((cell) => (
-              <td className='border-2 border-text-primary/50 p-4' key={cell.id}>
-                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-              </td>
+          </thead>
+          <motion.tbody
+            variants={{
+              initial: {},
+              current: {
+                transition: {
+                  delay: 0.08,
+                  delayChildren: stagger(0.08)
+                }
+              }
+            }}
+          >
+            {table.getRowModel().rows.map((row) => (
+              <motion.tr
+                variants={{
+                  initial: {
+                    opacity: 0,
+                    y: 10
+                  },
+                  current: {
+                    opacity: 1,
+                    y: 0,
+                    transition: {
+                      type: 'spring',
+                      stiffness: 300,
+                      damping: 24
+                    }
+                  }
+                }}
+                key={row.id}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <td
+                    className='border-2 border-text-primary/50 p-4'
+                    key={cell.id}
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </motion.tr>
             ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+          </motion.tbody>
+        </motion.table>
+      )}
+    </AnimatePresence>
   )
 }
 
