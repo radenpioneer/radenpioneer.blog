dbf6f7746adde173f7175b61bfa999300600bdb3
radenpioneer <bohrium.bearded_64@icloud.com>
2026-02-02 18:27:33 +0000

    add sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import cloudflare from '@astrojs/cloudflare'
+import alpinejs from '@astrojs/alpinejs'
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [alpinejs()],
+
   adapter: cloudflare({
     imageService: 'compile'
   }),
+
   image: {
     layout: 'constrained'
   }
