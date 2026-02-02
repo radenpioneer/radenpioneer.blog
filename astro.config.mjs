140d92a3c05982f609b08c534f3e8a3671af06fd
radenpioneer <bohrium.bearded_64@icloud.com>
2026-02-02 17:27:56 +0000

    setup formatter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from 'astro/config'
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
-    imageService: "compile"
+    imageService: 'compile'
   })
-});
+})
